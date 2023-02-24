d23a4a2dd988db87361455a4e79e121ae4e342c5
ducmin13 <ngoducminh1305@gmail.com>
2023-02-25 00:35:56 +0700

    chuc nang dang nhap

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'App\Http\Controllers\HomeController@index');
 
 
+
+
 //BE
 Route::get('/laravel/php/admin','App\Http\Controllers\AdminController@index');
+Route::get('/laravel/php/dashboard','App\Http\Controllers\AdminController@show_dashboard');
+Route::post('/laravel/php/admin-dashboard','App\Http\Controllers\AdminController@dashboard');
+Route::get('/laravel/php/logout','App\Http\Controllers\AdminController@logout');
